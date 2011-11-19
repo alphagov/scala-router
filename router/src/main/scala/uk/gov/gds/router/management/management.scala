ebf96b0ac2ba15bef336598e1ff1b85458a27caa
Matthew Wall <mat.x.wall@gmail.com>
2011-11-19 18:55:13 +0000

    Fixed threading issue found in management metrics. ConcurrentModificationException was possible. It's not now.

--- router/src/main/scala/uk/gov/gds/router/management/management.scala
+++ router/src/main/scala/uk/gov/gds/router/management/management.scala
@@ -5,10 +5,10 @@ import com.gu.management._
 import com.gu.management.ManagementPage
 import com.gu.management.Metric
 import request.RequestLoggingFilter
-import scala.collection.mutable.{Map => MutableMap}
 import javax.servlet.http.HttpServletRequest
 import uk.gov.gds.router.repository.application.Applications
 import uk.gov.gds.router.model.Route
+import java.util.concurrent.ConcurrentHashMap
 
 @Singleton
 class RouterRequestLoggingFilter extends RequestLoggingFilter(metric = Requests, shouldLogParametersOnNonGetRequests = true)
@@ -22,20 +22,18 @@ object Requests extends TimingMetric("requests")
 
 object ApplicationMetrics {
 
-  private val metrics = MutableMap[String,  TimingMetric]()
+  private val metrics = new ConcurrentHashMap[String, TimingMetric]()
 
-  def all = Applications.all.map(app => metric(app.application_id) )
+  def all = Applications.all.map(app => metric(app.application_id))
 
   def time[T](route: Route, block: => T) = metric(route.application.application_id).measure(block)
 
-  def metric(applicationId: String) = {
-    metrics.get(applicationId) match {
-      case Some(metric) => metric
-      case None =>
-        val metric = new TimingMetric(applicationId)
-        metrics(applicationId) = metric
-        metric
-    }
+  def metric(applicationId: String) = Option(metrics.get(applicationId)) match {
+    case Some(metric) => metric
+    case None =>
+      val metric = new TimingMetric(applicationId)
+      metrics.put(applicationId, metric)
+      metric
   }
 }
 
@@ -44,10 +42,10 @@ class ApplicationStatusPage(metrics: Seq[Metric]) extends ManagementPage {
 
   def get(req: HttpServletRequest) = XmlResponse(
     <status>
-      {metrics map { _.toXml }}
+      {metrics map {_.toXml}}
       <applications>
-        { ApplicationMetrics.all map { _.toXml } }
-      </applications>
+      {ApplicationMetrics.all map { _.toXml }}
+    </applications>
     </status>)
 }
 
