d8932ca5ace099eb51466c50b469b283911ec3a1
Craig R Webster <craig@barkingiguana.com>
2011-11-22 14:55:12 +0000

    Extract instance variable to method

--- lib/router/restful_resource_collection.rb
+++ lib/router/restful_resource_collection.rb
@@ -1,26 +1,29 @@
 module Router
   class RestfulResourceCollection
+    attr_accessor :http_client
+    private :http_client=, :http_client
+
     def collection_url(id); raise "Not implemented"; end
     def id_attribute; raise "Not implemented"; end
 
     def initialize(http_client)
-      @http_client = http_client
+      self.http_client = http_client
     end
 
     def create options
-      @http_client.post collection_url(options[id_attribute]), without_id(options)
+      http_client.post collection_url(options[id_attribute]), without_id(options)
     end
 
     def update options
-      @http_client.put collection_url(options[id_attribute]), without_id(options)
+      http_client.put collection_url(options[id_attribute]), without_id(options)
     end
 
     def delete id
-      @http_client.delete collection_url(id)
+      http_client.delete collection_url(id)
     end
 
     def find id
-      @http_client.get collection_url(id)
+      http_client.get collection_url(id)
     rescue NotFound
       nil
     end
