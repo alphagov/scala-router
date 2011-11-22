34e24ccc5242e9c811b58a81fca27c6c5064c7a0
Craig R Webster <craig@barkingiguana.com>
2011-11-22 14:32:41 +0000

    Whitespace

--- lib/router/http_client.rb
+++ lib/router/http_client.rb
@@ -4,14 +4,13 @@ require 'json'
 require 'null_logger'
 
 module Router
-
   class HttpClient
     def initialize(base_url, logger = nil)
       @base_url = base_url
       @logger = logger || NullLogger.instance
       @logger.debug "Base url: #{@base_url}"
     end
-    
+
     def router_url(partial_uri)
       URI.parse(@base_url + partial_uri)
     end
@@ -32,27 +31,27 @@ module Router
     def get(partial_uri)
       do_request Net::HTTP::Get, partial_uri
     end
-    
+
   protected
     def do_request(verb, partial_uri, form_data = nil)
       uri = router_url(partial_uri)
       request = verb.new(uri.path)
       request.form_data = form_data if form_data
-      @logger.debug "#{verb::METHOD}: #{uri} #{form_data.inspect}"      
-      response = Net::HTTP.new(uri.host, uri.port).start do |http| 
+      @logger.debug "#{verb::METHOD}: #{uri} #{form_data.inspect}"
+      response = Net::HTTP.new(uri.host, uri.port).start do |http|
         http.request(request)
       end
-      @logger.debug "Router responded with status: #{response.code}"      
+      @logger.debug "Router responded with status: #{response.code}"
       raise_on_error(response)
       parse_json(response.body) if is_json?(response)
     end
-    
+
     def is_json?(response)
       response.body and
         response.body.size > 0 and
         response.body[0] == '{'
     end
-    
+
     def parse_json(raw_json)
       @logger.debug "Parsing response as JSON"
       parsed = Hash[
@@ -61,12 +60,12 @@ module Router
       @logger.debug "Response parsed to #{parsed.inspect}"
       parsed
     end
-    
+
     def raise_on_error(response)
       case response.code.to_i
       when 409 then raise Conflict.new("Conflict", response)
       when 404 then raise NotFound.new("Not found", response)
-      when 400..599 then 
+      when 400..599 then
         raise RemoteError.new("Remote error", response)
       else
         response
