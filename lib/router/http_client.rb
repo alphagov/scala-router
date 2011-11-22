aadfbdc316291df72c57d405563ea1157d714389
Craig R Webster <craig@barkingiguana.com>
2011-11-22 14:53:24 +0000

    Extract instance variable to method

--- lib/router/http_client.rb
+++ lib/router/http_client.rb
@@ -3,10 +3,13 @@ module Router
     attr_accessor :base_url
     private :base_url=, :base_url
 
+    attr_accessor :logger
+    private :logger=, :logger
+
     def initialize(base_url, logger = nil)
       self.base_url = base_url
-      @logger = logger || NullLogger.instance
-      @logger.debug "Base url: #{@base_url}"
+      self.logger = logger || NullLogger.instance
+      logger.debug "Base url: #{base_url}"
     end
 
     def router_url(partial_uri)
@@ -35,11 +38,11 @@ module Router
       uri = router_url(partial_uri)
       request = verb.new(uri.path)
       request.form_data = form_data if form_data
-      @logger.debug "#{verb::METHOD}: #{uri} #{form_data.inspect}"
+      logger.debug "#{verb::METHOD}: #{uri} #{form_data.inspect}"
       response = Net::HTTP.new(uri.host, uri.port).start do |http|
         http.request(request)
       end
-      @logger.debug "Router responded with status: #{response.code}"
+      logger.debug "Router responded with status: #{response.code}"
       raise_on_error(response)
       parse_json(response.body) if is_json?(response)
     end
@@ -51,11 +54,11 @@ module Router
     end
 
     def parse_json(raw_json)
-      @logger.debug "Parsing response as JSON"
+      logger.debug "Parsing response as JSON"
       parsed = Hash[
         JSON.parse(raw_json).map {|k,v| [k.to_sym, v]}
       ]
-      @logger.debug "Response parsed to #{parsed.inspect}"
+      logger.debug "Response parsed to #{parsed.inspect}"
       parsed
     end
 
