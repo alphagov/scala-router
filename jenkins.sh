a032c1b8729b51a9e138c59ece396d522faa3ce3
psd <psd@whatfettle.com>
2012-05-25 11:00:53 +0100

    clear then md5sum the warfile passed to the 'Deploy Router' project

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 set -x -e
+
+artifact="${WORKSPACE:?}}/router/router/target/scala-2.9.1/router_2.9.1-0.1.0-SNAPSHOT.war"
+rm -f "$artifact"
+
 router/jenkins.sh
 client/jenkins.sh
 integration/jenkins.sh
+
+echo "post-build artifact stashed by Jenkins for the 'Deploy Router' project:"
+md5sum "$artifact"
