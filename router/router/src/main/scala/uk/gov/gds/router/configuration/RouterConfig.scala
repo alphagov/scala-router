a0c01bbcf3b63a07af7fdc09a8aa45ab4eb0dc64
Anna Shipman <anna.shipman@digital.cabinet-office.gov.uk>
2012-05-29 15:14:25 +0100

    removing code to find host as this will now be passed through as part of the route. Also removing test

--- router/router/src/main/scala/uk/gov/gds/router/configuration/RouterConfig.scala
+++ router/router/src/main/scala/uk/gov/gds/router/configuration/RouterConfig.scala
@@ -6,9 +6,4 @@ object RouterConfig {
 
   def databaseName: String = Config(name = "mongo.database.name", default = "router-dev")
 
-  def getPrefixForHost(serverName: String): String = {
-    val hostKey = "host-" + serverName
-    Config(name = hostKey, default = "/govuk/")       // remove the default after puppet config has been rolled out
-  }
-
 }
